8d1ca515b119a8afa97527e1bfb3e0cf0034af33
Armok <Armokthespideroverlord@gmail.com>
2026-05-29 02:18:45 -0400

    Reworked Rocket Thruster animation

--- tech/jetpack/r_jetpack.lua
+++ tech/jetpack/r_jetpack.lua
@@ -1,13 +1,6 @@
-require "/scripts/vec2.lua"
 function init()
-local b = mcontroller.boundBox()
-  animator.setParticleEmitterOffsetRegion("jetpackParticles", {b[1], b[2] + 0.2, b[3], b[2] + 0.3})
-  self.jetpackSpeed = config.getParameter("jetpackSpeed")
-  self.jetpackControlForce = config.getParameter("jetpackControlForce")
-  self.energyUsagePerSecond = config.getParameter("energyUsagePerSecond")
-self.holdingJump = false
-self.active = false
-  self.lastYVel = mcontroller.yVelocity()
+  self.holdingJump = false
+  self.active = false
 end
 
 function input(args)
@@ -17,7 +10,7 @@ function input(args)
     self.holdingJump = false
   end
 
-  if args.moves["jump"] and not status.statPositive("activeMovementAbilities") and not mcontroller.canJump() and not self.holdingJump then
+  if args.moves["jump"] and not mcontroller.canJump() and not self.holdingJump then
     return "jetpack"
   else
     return nil
@@ -25,28 +18,21 @@ function input(args)
 end
 
 function update(args)
-
-
-	if args.moves["jump"] and mcontroller.jumping() then
-		self.holdingJump = true
-	elseif not args.moves["jump"] then
-		self.holdingJump = false
-	end
   local action = input(args)
-local jetpack = args.moves["jump"] and (args.moves["down"] and mcontroller.yVelocity() <= -40 or not args.moves["down"]) and not mcontroller.canJump() and not self.holdingJump
-  if jetpack and status.consumeResource("energy", self.energyUsagePerSecond * args.dt) then
+  local jetpackSpeed = config.getParameter("jetpackSpeed")
+  local jetpackControlForce = config.getParameter("jetpackControlForce")
+  local energyUsagePerSecond = config.getParameter("energyUsagePerSecond")
+
+  if action == "jetpack" and status.consumeResource("energy", energyUsagePerSecond * args.dt) then
     animator.setAnimationState("jetpack", "on")
-    mcontroller.controlApproachYVelocity(self.jetpackSpeed, self.jetpackControlForce)
+    mcontroller.controlApproachYVelocity(jetpackSpeed, jetpackControlForce)
 
     if not self.active then
       animator.playSound("activate")
     end
     self.active = true
-    animator.setParticleEmitterActive("jetpackParticles", true)
   else
     self.active = false
     animator.setAnimationState("jetpack", "off")
-    animator.setParticleEmitterActive("jetpackParticles", false)
   end
-  self.lastYVel = mcontroller.yVelocity()
 end
